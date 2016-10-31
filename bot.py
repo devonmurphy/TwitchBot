e8694e19379363d7b3ce5b6ce3f6abcc6a73afbb
Devon Murphy <devonmurphy1@gmail.com>
2016-10-30 17:28:10 -0700

    Initial commit of bot

---
+++ bot.py
@@ -0,0 +1,45 @@
+import re
+from time import sleep
+import socket
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+NICK = "yxbljab"
+PASS = "oauth:joliitq9h7efzgpd11wp52vwt5lrk3"
+CHAN = "#yxbljab"
+RATE = (20 / 30)
+CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+def main_loop():
+    try:
+        s = socket.socket()
+        s.connect((HOST, PORT))
+        s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+        s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+        s.send("JOIN {}\r\n".format(CHAN).encode("utf-8"))
+        connected = True
+    except Exception as e:
+        print(str(e))
+        connected = False
+
+    while connected:
+        response = s.recv(1024).decode("utf-8")
+        if response == "PING :tmi.twitch.tv\r\n":
+            s.send("PONG :tmi.twitch.tv\r\n".encode())
+            print("PONG")
+        else:
+            username = re.search(r"\w+", response).group(0)
+            message = CHAT_MSG.sub("", response)
+
+            if message == "!test\r\n":
+                print "correct message"
+                chat(s, "Testing command received!")
+
+            print(username + ": " + message)
+        sleep(1)
+
+def chat(sock, msg):
+    sock.send("PRIVMSG {} :{}\r\n".format(CHAN, msg).encode())
+
+if __name__ == "__main__":
+    main_loop()
