b7ccde290b7691cc077730527d6702f88b7bd6a4
Devon Murphy <devonmurphy1@gmail.com>
2016-10-30 17:46:05 -0700

    added up down left right commands

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import socket
 HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "yxbljab"
-PASS = "oauth:joliitq9h7efzgpd11wp52vwt5lrk3"
+PASS = "oauth:"
 CHAN = "#yxbljab"
 RATE = (20 / 30)
 CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
@@ -30,13 +30,21 @@ def main_loop():
         else:
             username = re.search(r"\w+", response).group(0)
             message = CHAT_MSG.sub("", response)
-
+            
+            if message == "!right\r\n":
+                print "right"
+            if message == "!left\r\n":
+                print "left"
+            if message == "!up\r\n":
+                print "up"
+            if message == "!down\r\n":
+                print "down"
+        print(username + ": " + message)
+"""
             if message == "!test\r\n":
                 print "correct message"
                 chat(s, "Testing command received!")
-
-            print(username + ": " + message)
-        sleep(1)
+"""
 
 def chat(sock, msg):
     sock.send("PRIVMSG {} :{}\r\n".format(CHAN, msg).encode())
